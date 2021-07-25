d85454dbc8f780f074d789b1001570dbdff4a93f
John Wall <5672618@qq.com>
2021-07-25 21:41:51 +0800

    update

--- tools/app/download/src/Main.scala
+++ tools/app/download/src/Main.scala
@@ -14,25 +14,35 @@ class Download(args: collection.Seq[String]) extends Command(args) {
 
     val endIndex = opt[Int](descr = "终止下载索引", default = Some(137))
 
+    val abc = opt[String](descr = "开始字母", default = Some("A"))
+
     def execute(): Unit = {
       val session = requests.Session()
 
-      for (index <- startIndex() to endIndex()) {
-        for (suffix <- List("AM.mp3", "BM.mp3", "CM.mp3")) {
-          val fileName = f"20Y$index%03d$suffix"
-          val url =
-            s"http://ftp2.budaedu.org/newGhosa/C007/T020Y/audio-low/$fileName"
-            
-          println(s"下载 $url")
-          os.write(
-            os.Path(inputDir()) / fileName,
-            session.get(
-              url,
-              proxy = ("127.0.0.1", 1080)
-            )
+      def downloadFile(fileName: String) = {
+        val url =
+          s"http://ftp2.budaedu.org/newGhosa/C007/T020Y/audio-low/$fileName"
+
+        println(s"下载 $url")
+        os.write(
+          os.Path(inputDir()) / fileName,
+          session.get(
+            url,
+            proxy = ("127.0.0.1", 1080)
           )
-        }
+        )
       }
+
+      if (startIndex() <= endIndex()) {
+        List("AM.mp3", "BM.mp3", "CM.mp3")
+          .dropWhile(_ != abc() + "M.mp3")
+          .foreach(suffix => downloadFile(f"20Y${startIndex()}%03d$suffix"))
+      }
+
+      for {
+        index <- (startIndex() + 1) to endIndex()
+        suffix <- List("AM.mp3", "BM.mp3", "CM.mp3")
+      } downloadFile(f"20Y$index%03d$suffix")
     }
   }
 
